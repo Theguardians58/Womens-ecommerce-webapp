bf02fc13420ca1efd42fd632b5c0f80698ec5808
Theguardians58 <125973002+Theguardians58@users.noreply.github.com>
2025-10-30 08:28:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,17 @@ import 'package:shearose/screens/welcome_screen.dart';
 import 'package:shearose/supabase/supabase_config.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://dljqpqvyqbylqjvwuluh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsanFwcXZ5cWJ5bHFqdnd1bHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMzAxODAsImV4cCI6MjA3NjYwNjE4MH0.T7tHYgoInQyAWSYPYMP5wxIp6Ew4CFJ8wF0qjaSGAF0',
+  );
+  runApp(MyApp());
+}
+        
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseConfig.initialize();
